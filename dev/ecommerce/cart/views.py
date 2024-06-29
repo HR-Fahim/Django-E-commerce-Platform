96a50a75e70232ae30654bcd61da9ef413529f85
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-06-30 00:16:55 +0600

    feat: Implement AJAX functionality to add product to cart
    The code changes in this commit add the functionality to add a product to the cart using AJAX. The `cart_add` view function in `views.py` is updated to handle the AJAX request for adding a product to the cart. The `product-info.html` template is modified to include a button that triggers the AJAX request when clicked. This enhancement improves the shopping experience by allowing users to add products to their cart without refreshing the page.
    
    Co-authored-by: Habibur Rahaman Fahim

--- dev/ecommerce/cart/views.py
+++ dev/ecommerce/cart/views.py
@@ -6,6 +6,8 @@ from store.models import Product
 
 from django.shortcuts import get_object_or_404
 
+from django.http import JsonResponse
+
 # Create your views here.
 
 def cart_summary(request):
@@ -16,16 +18,19 @@ def cart_add(request):
 
     cart = Cart(request)
 
-    if request.POST.get('action') == 'POST':
+    if request.POST.get('action') == 'post':
 
         product_id = int(request.POST.get('product_id'))
+        
         product_quantity = int(request.POST.get('product_quantity'))
 
         product = get_object_or_404(Product, id=product_id)
 
         cart.add(product=product, product_qty=product_quantity)
 
+        response = JsonResponse({'The product is called:': product.title, ' and the quantity is:': product_quantity})
 
+        return response
 
 def cart_delete(request):
 
