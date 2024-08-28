268aa1f62ea3eb23278784fc9c638a94e38a382d
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-08-28 07:05:12 +0600

    feat: Improve delete functionality in Cart class
    This commit improves the delete functionality in the Cart class. It adds a new method called `delete` that allows for removing a product from the cart. The method takes the product ID as a parameter and deletes the corresponding entry from the cart dictionary. This enhancement enhances the user experience by providing the ability to remove products from the cart.
    
    Co-authored-by: Habibur Rahaman Fahim

--- dev/ecommerce/cart/views.py
+++ dev/ecommerce/cart/views.py
@@ -40,7 +40,19 @@ def cart_add(request):
 
 def cart_delete(request):
 
-    pass
+    cart =  Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity, 'total': cart.get_total()})
+
+        return response
 
 def cart_update(request):
 
