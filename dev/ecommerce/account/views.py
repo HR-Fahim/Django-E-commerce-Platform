b1911391074d51bdf10a98f3db64e493334cce2f
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-09-02 09:05:33 +0600

    feat: Add email verification routes and templates

--- dev/ecommerce/account/views.py
+++ dev/ecommerce/account/views.py
@@ -2,15 +2,47 @@ from django.shortcuts import redirect, render
 
 from .forms import CreateUserForm
 
-# Create your views here.
+from django.contrib.sites.shortcuts import get_current_site
+
+from .token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+
+from django.utils.encoding import force_bytes, force_str
+
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 
 def register(request):
     form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('store')
+
+            # form.save()
+            # return redirect('store')
+
+            user = form.save()
+
+            user.is_active = False
+            user.save()
+
+            # Email verification setup
+
+            current_site = get_current_site(request)
+
+            subject = 'Activate your account'
+
+            message = render_to_string('account/registration/email-verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            })
+
+            user.email_user(subject, message)
+
+            return redirect('email-verification-sent')
         
     context = {'form': form}
 
