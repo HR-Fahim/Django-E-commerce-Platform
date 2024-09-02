7927bbd8cc02f4bdce6991d48cfd230dc69b0bc8
Habibur Rahaman Fahim <hrfprofessional@gmail.com>
2024-09-03 04:27:26 +0600

    feat: Update email verification templates and routes

--- dev/ecommerce/ecommerce/settings.py
+++ dev/ecommerce/ecommerce/settings.py
@@ -142,4 +142,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email setup
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'hrhacker1260@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'Rahaman0172650'
